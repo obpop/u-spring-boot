bb4d83ef74b2e679e3019a876f532e8d01d22ad5
Oliver Berggren <oliverberggren00@gmail.com>
2024-05-24 00:52:10 +0200

    Initial project setup from Spring Initializr

---
+++ src/main/java/com/ll/spring/USpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ll.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(USpringBootApplication.class, args);
+	}
+
+}
